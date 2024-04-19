b3b32d7c2df413922d1a35319c54e2431bcb429b
Canrad <1517807724@qq.com>
2024-04-19 18:39:41 +0800

    test(*): 添加string的测试

---
+++ test/test-main.h
@@ -0,0 +1,9 @@
+#ifndef LIBCA_TEST_MAIN_H
+#define LIBCA_TEST_MAIN_H
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest/doctest.h>
+// #include <libca/libca.h>
+
+
+#endif /* ! LIBCA_TEST_MAIN_H */
