d63f1f91417ba7f020e2a409dedb8bab36a2c852
Canrad <1517807724@qq.com>
2024-06-29 15:27:36 +0800

    refactor(*): 重构构建项目脚本

--- src/xmake.lua
+++ src/xmake.lua
@@ -11,71 +11,79 @@ target("ca")
     -- add_packages("spdlog")
     add_links("mysqlclient")
 
-target("test-core")
+target("ca-core")
+    set_kind("shared")
+    add_includedirs("../src", {public = true})
+    add_files("libca/core/*.cpp|test-*.cpp")
+
+target("test-ca-core")
     set_kind("binary")
     add_defines("DEBUG")
     -- doctest main
     add_defines("DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN")
 
     add_packages("doctest")
-    add_packages("trompeloeil")
-    add_packages("nanobench")
-    add_packages("spdlog")
-
-    add_includedirs("../src")
 
     add_files("libca/core/test-*.cpp")
+    add_deps("ca-core")
+    add_links("ca-core")
 
-    add_deps("ca")
-    add_links("ca")
+target("ca-utility")
+    set_kind("shared")
+    add_includedirs("../src", {public = true})
+    add_files("libca/utility/*.cpp|test-*.cpp")
 
-target("test-utility")
+target("test-ca-utility")
     set_kind("binary")
     add_defines("DEBUG")
     -- doctest main
     add_defines("DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN")
 
     add_packages("doctest")
-    add_packages("trompeloeil")
-    add_packages("nanobench")
-    add_packages("spdlog")
-
-    add_includedirs("../src")
 
     add_files("libca/utility/test-*.cpp")
+    add_deps("ca-core")
+    add_deps("ca-utility")
+    add_links("ca-core")
+    add_links("ca-utility")
 
-    add_deps("ca")
-    add_links("ca")
+target("ca-database")
+    set_kind("shared")
+    add_includedirs("../src", {public = true})
+    add_files("libca/database/*.cpp|test-*.cpp")
+    add_links("mysqlclient")
 
-
-target("test-database")
+target("test-ca-database")
     set_kind("binary")
     add_defines("DEBUG")
     -- doctest main
     add_defines("DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN")
 
     add_packages("doctest")
-    add_packages("trompeloeil")
-    add_packages("nanobench")
-    add_packages("spdlog")
 
-    add_includedirs("../src")
-    
     add_files("libca/database/test-*.cpp")
+    add_deps("ca-core")
+    add_deps("ca-database")
+    add_links("ca-core")
+    add_links("ca-database")
 
-    add_deps("ca")
-    add_links("ca")
+target("ca-log")
+    set_kind("shared")
+    add_includedirs("../src", {public = true})
+    add_files("libca/log/*.cpp|test-*.cpp")
 
-target("test-log")
+target("test-ca-log")
     set_kind("binary")
     add_defines("DEBUG")
     -- doctest main
     add_defines("DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN")
 
-    add_includedirs("../src")
-    
-    add_files("libca/log/test-*.cpp")
+    add_packages("doctest")
 
-    add_deps("ca")
-    add_links("ca")
+    add_files("libca/log/test-*.cpp")
+    add_deps("ca-core")
+    add_deps("ca-log")
+    add_links("ca-core")
+    add_links("ca-log")
+
 
