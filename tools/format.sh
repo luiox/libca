be19c58d2f15f20c5d90f78f8d31805be1706cea
Canrad <1517807724@qq.com>
2025-01-04 20:49:24 +0800

    Update format.sh

--- tools/format.sh
+++ tools/format.sh
@@ -3,4 +3,4 @@ cd ..
 # 获取上一级目录的路径
 root_dir=$(pwd)
 # 使用clang-format格式化代码
-find "$root_dir" -type f \( -iname '*.h' -o -iname '*.cpp' -o -iname '*.hpp' -o -iname '*.hh' \) -exec clang-format -i {} \;
+find "$root_dir" -type f \( -iname '*.h' -o -iname '*.c' -o -iname '*.hpp' -o -iname '*.cpp' \) -exec clang-format -i {} \;
