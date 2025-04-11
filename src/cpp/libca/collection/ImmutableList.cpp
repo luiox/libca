ce9bd44b646a3c7e326304e4b1d8aa0832693aee
Canrad <1517807724@qq.com>
2025-04-11 22:43:49 +0800

    添加不可变列表

--- src/cpp/libca/collection/ImmutableList.cpp
+++ src/cpp/libca/collection/ImmutableList.cpp
@@ -1,11 +1,15 @@
 #include "ImmutableList.hpp"
 
 #include "libca/test/Test.hpp"
+using namespace ca;
 
 TEST_CASE("ImmutableList")
 {
-    auto list = ca::ImmutableList<int, 5>::of(1, 2, 3, 4, 5);
-    REQUIRE_TRUE(list.size() == 5);
-    REQUIRE_TRUE(list.get(0) == 1);
+    // auto list = ca::ImmutableList::of(1, 2, 3, 4, 5);
+    // REQUIRE_TRUE(list.size() == 5);
+    // REQUIRE_TRUE(list.get(0) == 1);
+
+    auto list = ImmutableList<int>::of(1, 2, 3);  
+    //auto list2 = ImmutableList<std::string>::of("hello", "world"); 
 
 }
