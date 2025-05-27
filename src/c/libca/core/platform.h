a21ac71c5024bfa6a62545d4fb982178566d27fb
Canrad <1517807724@qq.com>
2025-05-27 22:18:00 +0800

    重新定义类型

---
+++ src/c/libca/core/platform.h
@@ -0,0 +1,24 @@
+#ifndef LIBCA_CORE_PLATFORM_H
+#define LIBCA_CORE_PLATFORM_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+typedef int8_t   i8_t;
+typedef uint8_t  u8_t;
+typedef int16_t  i16_t;
+typedef uint16_t u16_t;
+typedef int32_t  i32_t;
+typedef uint32_t u32_t;
+typedef int64_t  i64_t;
+typedef uint64_t u64_t;
+
+typedef char bool_t;
+
+#define bool_true 1
+#define bool_false 0
+
+// size_t is defined in stddef.h
+
+
+#endif   // !LIBCA_CORE_PLATFORM_H
