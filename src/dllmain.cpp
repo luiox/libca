cdbedaecd1da6ffd158bec6bdd038b191d6af087
Canrad <1517807724@qq.com>
2025-10-27 23:04:16 +0800

    Update dllmain.cpp

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -1,2 +1,20 @@
-#include "libca/base/Platform.hpp"
+#include "base/Platform.hpp"
 
+#ifdef _WIN32
+
+int APIENTRY DllMain(HMODULE hModule,
+                      DWORD  ul_reason_for_call,
+                      LPVOID lpReserved
+)
+{
+    switch (ul_reason_for_call) {
+    case DLL_PROCESS_ATTACH:
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
+#endif
