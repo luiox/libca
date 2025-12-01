d9779365a2c735c672fddfbb7b1322a77c52c172
Canrad <1517807724@qq.com>
2025-12-01 15:38:40 +0800

    移除多余功能

--- src/base/compiler_helper.c
+++ src/base/compiler_helper.c
@@ -1,11 +1,11 @@
 #include "compiler_helper.h"
-#include <stdio.h>
+#include "debug.h"
 
 void print_compiler_info()
 {
-    printf("Compiler: %s %d.%d\n", COMPILER_NAME, COMPILER_VERSION_MAJOR, COMPILER_VERSION_MINOR);
-    printf("C Standard: %s\n", C_STANDARD_NAME);
-    printf("C++ Standard: %s\n", CPP_STANDARD_NAME);
-    printf("Platform: %s\n", PLATFORM_NAME);
-    printf("Architecture: %s\n", ARCH_NAME);
+    ca_dprintf("Compiler: %s %d.%d\n", COMPILER_NAME, COMPILER_VERSION_MAJOR, COMPILER_VERSION_MINOR);
+    ca_dprintf("C Standard: %s\n", C_STANDARD_NAME);
+    ca_dprintf("C++ Standard: %s\n", CPP_STANDARD_NAME);
+    ca_dprintf("Platform: %s\n", PLATFORM_NAME);
+    ca_dprintf("Architecture: %s\n", ARCH_NAME);
 }
--- src/base/debug.h
+++ src/base/debug.h
@@ -94,4 +94,35 @@ void debug_print(const char* fmt, ...);
         {}
 #endif
 
+// #define C_ASSERT_STATIC(condition) \
+//     typedef char c_assert_##__LINE__[(condition) ? 1 : -1]
+
+// #define C_ASSERT_STATIC(condition) \
+//     struct c_assert_##__LINE__ { unsigned int : (condition) ? 1 : 0; }
+
+#ifdef __cplusplus
+
+#    ifdef NDEBUG
+
+#        define ca_assert(expr) static_cast<void>(0)
+
+#    else
+
+#        define ca_assert(expr) \
+            static_cast<void>(expr) ? 0 : (::ca::assertion_failed(#expr, __FILE__, __LINE__), 0)
+
+#    endif   // NDEBUG
+
+
+#endif   // __cplusplus
+
+#ifndef NDEBUG
+
+#    define ca_assert(expr) ((void)0)
+#else
+
+#    define ca_assert(expr) ((void)0)
+
+#endif   // NDEBUG
+
 #endif   // !MYLIB_BASE_DEBUG_H
