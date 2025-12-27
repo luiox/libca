60e2511f1deeb6389d43b6e30677fecdd250c028
Canrad <1517807724@qq.com>
2025-12-27 16:13:42 +0800

    移除多余的类

--- src/base/Wrapper.cpp
+++ src/base/Wrapper.cpp
@@ -7,22 +7,5 @@
 using namespace ca::test;
 using namespace ca;
 
-TEST_CASE(RefTest)
-{
-    int v = 0;
-    Ref ref(v);
-    ASSERT_EQUAL(0, ref);
-    ref = 1;
-    ASSERT_EQUAL(1, ref);
-}
-
-TEST_CASE(BoxTest)
-{
-    int v = 0;
-    Box box(v);
-    ASSERT_EQUAL(0, *box);
-    *box = 1;
-    ASSERT_EQUAL(1, *box);
-}
 
 #endif
