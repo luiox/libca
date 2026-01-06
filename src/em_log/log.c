871642bfc128fcc6ae8a4e35148c1f9d00b7620d
Canrad <1517807724@qq.com>
2026-01-06 16:48:27 +0800

    Update log.c

--- src/em_log/log.c
+++ src/em_log/log.c
@@ -17,8 +17,8 @@ static bool g_log_cs_init = false;
 #define EM_CPU_EXIT_CRITICAL() LeaveCriticalSection(&g_log_cs)
 
 #else
-#define EM_CPU_ENTER_CRITICAL() (void(0))
-#define EM_CPU_EXIT_CRITICAL() (void(0))
+#define EM_CPU_ENTER_CRITICAL() 
+#define EM_CPU_EXIT_CRITICAL() 
 #endif
 
 #include <stdio.h>
