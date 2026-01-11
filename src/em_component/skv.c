36c36b0b3f59da9c33c7ae7ce801279f24ae37b7
Canrad <1517807724@qq.com>
2026-01-11 16:48:55 +0800

    重新设计skv

--- src/em_component/skv.c
+++ src/em_component/skv.c
@@ -1,177 +1,190 @@
-/**
- * @file skv.c
- * @author canrad (1517807724@qq.com)
- * @brief Simple Key Value format implementation
- * @version 0.1
- * @date 2025-07-28
- * 
- * @copyright Copyright (c) 2025
- * 
- */
-
 #include "skv.h"
-#include "../em_base/string_util.h"
+#include "../em_util/crc.h"
+#include <string.h>
 
-void skv_init(skv_t* skv, skv_kv_t* kvs, u32 num) {
-    skv->kvs = kvs;
-    skv->num = num;
+static skv_port_t* g_skv_port = NULL;
+
+void skv_bind_port(skv_port_t* port)
+{
+    g_skv_port = port;
 }
 
-bool skv_put(skv_t* skv, const char* key, const char* value) {
-    // 查找是否已存在该key
-    for (u32 i = 0; i < skv->num; i++) {
-        if (str_cmp(skv->kvs[i].key, key, SKV_MAX_KEY_LEN) == 0) {
-            // 找到已存在的key，更新value
-            str_cpy(skv->kvs[i].value, value, SKV_MAX_VALUE_LEN);
-            return true;
-        }
-    }
-    
-    // 查找空槽位
-    for (u32 i = 0; i < skv->num; i++) {
-        if (skv->kvs[i].key[0] == '\0') {
-            // 找到空槽位，插入新键值对
-            str_cpy(skv->kvs[i].key, key, SKV_MAX_KEY_LEN);
-            str_cpy(skv->kvs[i].value, value, SKV_MAX_VALUE_LEN);
-            return true;
-        }
-    }
-    
-    // 没有空槽位
-    return false;
+bool skv_port_is_registered()
+{
+    return g_skv_port != NULL;
 }
 
-bool skv_get(skv_t* skv, const char* key, char* value) {
-    for (u32 i = 0; i < skv->num; i++) {
-        if (str_cmp(skv->kvs[i].key, key, SKV_MAX_KEY_LEN) == 0) {
-            str_cpy(value, skv->kvs[i].value, SKV_MAX_VALUE_LEN);
-            return true;
-        }
+void skv_init(skv_t* skv, u32 start_addr)
+{
+    skv->total_size = 0;
+    skv->num        = 0;
+    skv->next_addr  = start_addr + 32;   // header区32字节
+    skv->start_addr = start_addr;
+
+    // 先尝试读取
+    bool valid = skv_read_header(skv);
+    if (!valid) {
+        // 如果header无效，初始化header
+        skv_write_header(skv);
     }
-    
-    // 未找到key
-    return false;
 }
 
-void skv_to_str(skv_t* skv, char* buf) {
-    char* p = buf;
-    
-    for (u32 i = 0; i < skv->num; i++) {
-        if (skv->kvs[i].key[0] != '\0') {
-            // 格式: key=value\n
-            char* key = skv->kvs[i].key;
-            char* value = skv->kvs[i].value;
-            
-            // 复制key
-            while (*key) {
-                *p++ = *key++;
-            }
-            
-            // 添加等号
-            *p++ = '=';
-            
-            // 复制value
-            while (*value) {
-                *p++ = *value++;
-            }
-            
-            // 添加换行符
-            *p++ = '\n';
-        }
+// 读取header
+// 如果返回false，表示header无效
+bool skv_read_header(skv_t* skv)
+{
+    u8 buf[32];
+    if (!g_skv_port->read(skv->start_addr, buf, 32)) {
+        return false;
     }
-    
-    // 字符串结束符
-    *p = '\0';
+    // 解析header
+    u32 magic = *((u32*)&buf[0]);
+    if (magic != SKV_MAGIC_NUMBER) {
+        return false;
+    }
+    skv->num        = *((u32*)&buf[4]);
+    skv->total_size = *((u32*)&buf[8]);
+    skv->next_addr  = *((u32*)&buf[12]);
+    return true;
 }
 
-void skv_from_str(skv_t* skv, char* buf) {
-    char* line = str_tok(buf, "\n");
-    
-    while (line != NULL) {
-        // 跳过空行和注释行
-        if (line[0] != '\0' && line[0] != '#' && line[0] != ';') {
-            // 查找等号
-            char* equal_pos = str_chr(line, '=');
-            if (equal_pos != NULL && equal_pos != line) {
-                // 分割key和value
-                *equal_pos = '\0';
-                char* key = line;
-                char* value = equal_pos + 1;
-                
-                // ?????
-                skv_put(skv, key, value);
-            }
-        }
-        
-        line = str_tok(NULL, "\n");
-    }
+bool skv_write_header(skv_t* skv)
+{
+    u8 buf[32] = {0};
+    // 填充header
+    *((u32*)&buf[0])  = SKV_MAGIC_NUMBER;
+    *((u32*)&buf[4])  = skv->num;
+    *((u32*)&buf[8])  = skv->total_size;
+    *((u32*)&buf[12]) = skv->next_addr;
+    return g_skv_port->write(skv->start_addr, buf, 32);
 }
+
+// 写入键值对
+i32 skv_put_item(skv_t* skv, skv_kv_item_t* item)
+{
+    u32 next_addr = skv->next_addr;
+    // 计算需要的空间
+    // 1byte item magic number(0x66) + 1byte key长度 + n bytes key + 1byte value类型 + 1byte
+    // value长度 + n bytes value + 2byte crc16
+    u32 needed_size = 1 + 1 + item->key_length + 1 + 1 + item->value_length + 2;
+    if (next_addr + needed_size > skv->start_addr + skv->total_size) {
+        // 空间不足
+        return -1;
+    }
+    // 计算crc16-modebus
+    u8 item_magic = SKV_ITEM_MAGIC;
+
+    u16 crc = CRC16_MODEBUS_START_VALUE;
+    // 计算item magic number
+    crc = crc16_modbus_ex(&item_magic, 1, crc);
+    // 计算key长度和key
+    crc = crc16_modbus_ex(&item->key_length, 1, crc);
+    crc = crc16_modbus_ex((u8*)item->key, item->key_length, crc);
+    // 计算value类型和value长度
+    crc = crc16_modbus_ex(&item->value_type, 1, crc);
+    crc = crc16_modbus_ex(&item->value_length, 1, crc);
+    // 计算value
+    crc = crc16_modbus_ex((u8*)item->value, item->value_length, crc);
+
+    // 写入数据
+    bool ret = true;
+    // 写入item magic number
+    ret &= g_skv_port->write(next_addr, &item_magic, 1);
+    next_addr += 1;
+    // 写入key
+    ret &= g_skv_port->write(next_addr, &item->key_length, 1);
+    next_addr += 1;
+    ret &= g_skv_port->write(next_addr, (u8*)item->key, item->key_length);
+    next_addr += item->key_length;
+
+    // 写入value
+    ret &= g_skv_port->write(next_addr, &item->value_type, 1);
+    next_addr += 1;
+    ret &= g_skv_port->write(next_addr, &item->value_length, 1);
+    next_addr += 1;
+    ret &= g_skv_port->write(next_addr, (u8*)item->value, item->value_length);
+    next_addr += item->value_length;
+
+    // 写入crc16
+    ret &= g_skv_port->write(next_addr, (u8*)&crc, 2);
+    next_addr += 2;
+
+    if(!ret){
+        // 有1个失败就回滚
+        return -1;
+    }
+
+    // 更新next_addr
+    skv->next_addr = next_addr;
+    skv->num++;
+    
+    return 0;
+}
+
+// 读取键值对
+i32 skv_get_item(skv_t* skv, skv_kv_item_t* item) 
+{
+    u32 addr = skv->start_addr + 32; // header区32字节
+    while (addr < skv->next_addr) {
+        u8 item_magic = 0;
+        // 读取item magic number
+        g_skv_port->read(addr, &item_magic, 1);
+        if (item_magic != SKV_ITEM_MAGIC) {
+            // 非法item，跳过
+            break;
+        }
+        addr += 1;
+        // 读取key长度
+        u8 key_length = 0;
+        g_skv_port->read(addr, &key_length, 1);
+        addr += 1;
+        // 读取key
+        char key_buf[SKV_MAX_KEY_LEN + 1] = {0};
+        g_skv_port->read(addr, (u8*)key_buf, key_length);
+        addr += key_length;
+
+        // 比较key
+        if (strcmp(key_buf, item->key) == 0) {
+            // 找到对应key，读取value
+            // 读取value类型
+            g_skv_port->read(addr, &item->value_type, 1);
+            addr += 1;
+            // 读取value长度
+            g_skv_port->read(addr, &item->value_length, 1);
+            addr += 1;
+            // 读取value
+            g_skv_port->read(addr, (u8*)item->value, item->value_length);
+            addr += item->value_length;
+            // 跳过crc16
+            addr += 2;
+            return 0;
+        } else {
+            // 跳过value类型和长度及value和crc16
+            u8 value_type = 0;
+            g_skv_port->read(addr, &value_type, 1);
+            addr += 1;
+            u8 value_length = 0;
+            g_skv_port->read(addr, &value_length, 1);
+            addr += 1 + value_length + 2; // value + crc16
+        }
+    }
+    // 未找到
+    return SKV_ERR_NOT_FOUND;
+}
+
+i32 skv_put_i32(skv_t* skv, const char* key, i32 value)
+{
+    return 0;
+}
+
 #if TEST_ENABLE
 
-#include "../em_test/test.h"
-#include <string.h>
+#    include "../em_test/test.h"
+#    include <string.h>
 
 TEST_CASE(skv_basic)
 {
-    skv_kv_t kvs[10];
-    skv_t    skv;
-    char     value[SKV_MAX_VALUE_LEN];
-
-    memset(kvs, 0, sizeof(kvs));
-    skv_init(&skv, kvs, 10);
-    
-    // ????
-    TEST_ASSERT(skv_put(&skv, "name", "skv") == true);
-    
-    // ????
-    TEST_ASSERT(skv_get(&skv, "name", value) == true);
-    TEST_ASSERT_EQUAL_STRING("skv", value);
-    
-    // ????
-    TEST_ASSERT(skv_put(&skv, "name", "skv_updated") == true);
-    
-    // ???????
-    TEST_ASSERT(skv_get(&skv, "name", value) == true);
-    TEST_ASSERT_EQUAL_STRING("skv_updated", value);
 }
 
-TEST_CASE(skv_serialization)
-{
-    skv_kv_t kvs[10];
-    skv_t    skv;
-    char     buf[1024];
-    char     value[SKV_MAX_VALUE_LEN];
-
-    memset(kvs, 0, sizeof(kvs));
-    skv_init(&skv, kvs, 10);
-    skv_put(&skv, "name", "skv_updated");
-    
-    // ???
-    skv_to_str(&skv, buf);
-    
-    // ??????
-    memset(kvs, 0, sizeof(kvs));
-    skv_init(&skv, kvs, 10);
-    
-    // ????
-    skv_from_str(&skv, buf);
-    
-    // ??????????
-    TEST_ASSERT(skv_get(&skv, "name", value) == true);
-    TEST_ASSERT_EQUAL_STRING("skv_updated", value);
-}
-
-TEST_CASE(skv_capacity)
-{
-    skv_kv_t kvs[3];
-    skv_t    skv;
-    memset(kvs, 0, sizeof(kvs));
-    skv_init(&skv, kvs, 3);
-    
-    TEST_ASSERT(skv_put(&skv, "k1", "v1") == true);
-    TEST_ASSERT(skv_put(&skv, "k2", "v2") == true);
-    TEST_ASSERT(skv_put(&skv, "k3", "v3") == true);
-    TEST_ASSERT(skv_put(&skv, "k4", "v4") == false);
-}
 
 #endif
--- src/em_component/skv.h
+++ src/em_component/skv.h
@@ -1,39 +1,84 @@
 /**
  * @file skv.h
  * @author canrad (1517807724@qq.com)
- * @brief Simple Key Value format
- *  目的是简化无文件系统，无操作系统下的配置存储
- *  面向flash这种情况下的程序配置管理
+ * @brief 无文件系统的eeprom下的键值存储
+ *  
  * @version 0.1
- * @date 2025-07-28
+ * @date 2026-01-11
  * 
- * @copyright Copyright (c) 2025
+ * @copyright Copyright (c) 2026
  * 
  */
 #ifndef SKV_H
 #define SKV_H
 
 #include "../em_base/datatype.h" 
-#include <stdbool.h>
 
-#define SKV_MAX_KEY_LEN 16
-#define SKV_MAX_VALUE_LEN 16
+typedef struct skv_port{
+    bool (*read)(u32 addr, u8* buf, u32 len);
+    bool (*write)(u32 addr, u8* buf, u32 len);
+}skv_port_t;
 
-typedef struct skv_kv {
-    char key[SKV_MAX_KEY_LEN];
-    char value[SKV_MAX_VALUE_LEN];
-}skv_kv_t;
+void skv_bind_port(skv_port_t* port);
+bool skv_port_is_registered();
+
+/*
+  存储设计：分为header区和kv区
+  header区(32字节)：
+  4bytes magic number + 4bytes number + 4bytes 总的eeprom大小 + 4bytes kv的数量 + 4bytes 下一个kv写入位置 + 12bytes 保留
+  kv区：
+  1byte item magic number(0x66) + 1byte key长度 + n bytes key + 1byte value类型 + 1byte value长度 + n bytes value + 2byte crc16
+ 
+  key最大是255字节的字符串
+  value最大是255字节的二进制数据
+  */
+#define SKV_MAX_KEY_LEN 255
+#define SKV_MAX_VALUE_LEN 255
+
+#define SKV_TYPE_U8 0x01
+#define SKV_TYPE_U16 0x02
+#define SKV_TYPE_U32 0x03
+#define SKV_TYPE_U64 0x04
+#define SKV_TYPE_I8 0x05
+#define SKV_TYPE_I16 0x06
+#define SKV_TYPE_I32 0x07
+#define SKV_TYPE_F32 0x08
+#define SKV_TYPE_F64 0x09
+#define SKV_TYPE_STRING 0x0A
+#define SKV_TYPE_BLOB 0x0B
+
+typedef struct skv_kv_item {
+    u8 key_length;
+    char* key;
+    u8 value_type;
+    u8 value_length;
+    void* value;
+}skv_kv_item_t;
+
+#define SKV_MAGIC_NUMBER 0x534B5631 // "SKV1"
+#define SKV_ITEM_MAGIC 0x66
+
+#define SKV_ERR_NOT_FOUND -1
 
 typedef struct skv {
-    skv_kv_t* kvs;
     u32 num;
+    u32 next_addr;
+    u32 total_size;
+    u32 start_addr;
 }skv_t;
 
-void skv_init(skv_t* skv, skv_kv_t* kvs, u32 num);
-bool skv_put(skv_t* skv, const char* key, const char* value);
-bool skv_get(skv_t* skv, const char* key, char* value);
-void skv_to_str(skv_t* skv, char* buf);
-void skv_from_str(skv_t* skv, char* buf);
+// 初始化skv结构体
+void skv_init(skv_t* skv, u32 start_addr);
+// 读取header
+// 如果返回false，表示header无效
+bool skv_read_header(skv_t* skv);
+// 写入header
+bool skv_write_header(skv_t* skv);
+// 写入键值对
+i32 skv_put_item(skv_t* skv, skv_kv_item_t* item);
+// 读取键值对
+i32 skv_get_item(skv_t* skv, skv_kv_item_t* item);
+
 
 
 #endif // !SKV_H
