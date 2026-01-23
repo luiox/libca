0aebed2affd68bc40d5a77b1276ffe033e6f38ae
Canrad <1517807724@qq.com>
2026-01-23 21:59:22 +0800

    实现string_util的str相关函数

--- src/em_base/string_util.c
+++ src/em_base/string_util.c
@@ -1,97 +1,27 @@
 #include "string_util.h"
-#include <string.h>
 
-// 添加静态变量用于str_tok函数的状态保持
-static char* strtok_pos = NULL;
-
-char* str_cpy(char* dest, const char* src, usize size)
+char char_to_lower(char c)
 {
-    if (size == 0)
-        return dest;
-
-    char* ret = dest;
-    usize i;
-
-    for (i = 0; i < size - 1 && src[i] != '\0'; i++) {
-        dest[i] = src[i];
+    if (c >= 'A' && c <= 'Z') {
+        return c - 'A' + 'a';
     }
-    dest[i] = '\0';   // 确保终止符
-
-    return ret;
+    return c;
 }
 
-// 字符串拼接
-char* str_cat(char* dest, const char* src, usize dest_max_size)
+char char_to_upper(char c)
 {
-    // 先找得到dest的\0
-    while(*dest !='\0'){
-        dest++;
+    if (c >= 'a' && c <= 'z') {
+        return c - 'a' + 'A';
     }
-    // 拼接
-    return str_cpy(dest, src, dest_max_size - (dest - dest));
-}
-
-bool hex_str_to_uint(const char* str, u32* out_value)
-{
-    if (!str || !out_value)
-        return false;
-
-    u32 value = 0;
-    while (*str) {
-        char c = *str;
-        u32  digit;
-
-        if (c >= '0' && c <= '9') {
-            digit = c - '0';
-        }
-        else if (c >= 'A' && c <= 'F') {
-            digit = 10 + (c - 'A');
-        }
-        else if (c >= 'a' && c <= 'f') {
-            digit = 10 + (c - 'a');
-        }
-        else {
-            return false;   // 非法字符
-        }
-
-        if (value > (UINT32_MAX - digit) / 16) {
-            return false;   // 溢出
-        }
-
-        value = value * 16 + digit;
-        str++;
-    }
-
-    *out_value = value;
-    return true;
-}
-
-
-void uint_to_hex_str(u32 value, char* out_str, size_t out_size)
-{
-    if (!out_str || out_size == 0)
-        return;
-
-    const char* digits = "0123456789ABCDEF";
-    usize       pos    = out_size - 1;
-    out_str[pos]       = '\0';   // 终止符
-
-    do {
-        pos--;
-        out_str[pos] = digits[value & 0xF];
-        value >>= 4;
-    } while (pos > 0 && value != 0);
-
-    // 如果还有空间，左移字符串
-    if (pos > 0) {
-        memmove(out_str, &out_str[pos], out_size - pos);
-    }
+    return c;
 }
 
 usize str_len(const char* str)
 {
-    if (!str) return 0;
-    
+    if (!str) {
+        return 0;
+    }
+
     usize len = 0;
     while (str[len] != '\0') {
         len++;
@@ -99,57 +29,191 @@ usize str_len(const char* str)
     return len;
 }
 
-int str_cmp(const char* s1, const char* s2, usize size)
+usize str_nlen(const char* str, usize max_len)
 {
-    if (!s1 || !s2) return -1;
+    if (!str) {
+        return 0;
+    }
+
+    usize len = 0;
+    while (len < max_len && str[len] != '\0') {
+        len++;
+    }
+    return len;
+}
+
+i32 str_cpy(char* dest, const char* src, usize size)
+{
+    if (!dest || !src || size == 0) {
+        return -1;
+    }
+
+    usize i;
+    for (i = 0; i < size - 1 && src[i] != '\0'; i++) {
+        dest[i] = src[i];
+    }
+    dest[i] = '\0';
+
+    // 如果源字符串没拷完，说明空间不足
+    if (src[i] != '\0') {
+        return -1;
+    }
+
+    return (i32)i;
+}
+
+i32 str_cat(char* dest, const char* src, usize dest_max_size)
+{
+    if (!dest || !src || dest_max_size == 0) {
+        return -1;
+    }
+
+    usize dest_len = str_nlen(dest, dest_max_size);
+    if (dest_len >= dest_max_size) {
+        return -1; // 目标缓冲区已经满了或者没有结束符
+    }
+
+    usize i;
+    usize remaining = dest_max_size - dest_len;
     
+    for (i = 0; i < remaining - 1 && src[i] != '\0'; i++) {
+        dest[dest_len + i] = src[i];
+    }
+    dest[dest_len + i] = '\0';
+
+    if (src[i] != '\0') {
+        return -1; // 没拷贝完
+    }
+
+    return (i32)(dest_len + i);
+}
+
+i32 str_cmp(const char* s1, const char* s2, usize size)
+{
+    if (!s1 || !s2 || size == 0) {
+        return 0;
+    }
+
     for (usize i = 0; i < size; i++) {
-        if (s1[i] != s2[i]) {
-            return (unsigned char)s1[i] - (unsigned char)s2[i];
-        }
-        if (s1[i] == '\0') {
-            break;
+        if (s1[i] != s2[i] || s1[i] == '\0') {
+            return (i32)((u8)s1[i] - (u8)s2[i]);
         }
     }
+
     return 0;
+}
+
+bool str_is_equal(const char* s1, const char* s2)
+{
+    if (s1 == s2) {
+        return true;
+    }
+    if (!s1 || !s2) {
+        return false;
+    }
+
+    while (*s1 && (*s1 == *s2)) {
+        s1++;
+        s2++;
+    }
+
+    return (*s1 == *s2);
 }
 
 char* str_find_ch(const char* str, char c)
 {
-    if (!str) return NULL;
-    
+    if (!str) {
+        return NULL;
+    }
+
     while (*str) {
         if (*str == c) {
             return (char*)str;
         }
         str++;
     }
+
+    // 考虑查找 \0 的情况
+    if (c == '\0') {
+        return (char*)str;
+    }
+
     return NULL;
 }
 
 char* str_find_str(const char* haystack, const char* needle)
 {
-    if (!haystack || !needle) return NULL;
-    
-    usize needle_len = str_len(needle);
-    if (needle_len == 0) return (char*)haystack;
-    
-    while (*haystack) {
-        if (str_cmp(haystack, needle, needle_len) == 0) {
-            return (char*)haystack;
-        }
-        haystack++;
+    if (!haystack || !needle) {
+        return NULL;
     }
+
+    if (*needle == '\0') {
+        return (char*)haystack;
+    }
+
+    for (const char* h = haystack; *h != '\0'; h++) {
+        if (*h == *needle) {
+            const char* h_sub = h;
+            const char* n_sub = needle;
+            while (*h_sub != '\0' && *n_sub != '\0' && *h_sub == *n_sub) {
+                h_sub++;
+                n_sub++;
+            }
+            if (*n_sub == '\0') {
+                return (char*)h;
+            }
+        }
+    }
+
     return NULL;
+}
+
+i32 str_trim(char* str)
+{
+    if (!str) {
+        return 0;
+    }
+
+    usize len = str_len(str);
+    if (len == 0) {
+        return 0;
+    }
+
+    // 去除尾部
+    i32 end = (i32)len - 1;
+    while (end >= 0 && (str[end] == ' ' || str[end] == '\t' || str[end] == '\n' || str[end] == '\r')) {
+        str[end] = '\0';
+        end--;
+    }
+
+    // 去除头部
+    usize start = 0;
+    while (str[start] != '\0' && (str[start] == ' ' || str[start] == '\t' || str[start] == '\n' || str[start] == '\r')) {
+        start++;
+    }
+
+    if (start > 0) {
+        usize new_len = 0;
+        usize i = start;
+        while (str[i] != '\0') {
+            str[new_len++] = str[i++];
+        }
+        str[new_len] = '\0';
+        return (i32)new_len;
+    }
+
+    return (i32)str_len(str);
 }
 
 void str_to_upper(char* str)
 {
-    if (!str) return;
-    
+    if (!str) {
+        return;
+    }
+
     while (*str) {
         if (*str >= 'a' && *str <= 'z') {
-            *str = *str - 'a' + 'A';
+            *str -= 'a' - 'A';
         }
         str++;
     }
@@ -157,11 +221,13 @@ void str_to_upper(char* str)
 
 void str_to_lower(char* str)
 {
-    if (!str) return;
-    
+    if (!str) {
+        return;
+    }
+
     while (*str) {
         if (*str >= 'A' && *str <= 'Z') {
-            *str = *str - 'A' + 'a';
+            *str += 'a' - 'A';
         }
         str++;
     }
@@ -169,206 +235,307 @@ void str_to_lower(char* str)
 
 void str_reverse(char* str)
 {
-    if (!str) return;
-    
+    if (!str) {
+        return;
+    }
+
     usize len = str_len(str);
-    if (len <= 1) return;
-    
-    usize i, j;
-    for (i = 0, j = len - 1; i < j; i++, j--) {
+    if (len <= 1) {
+        return;
+    }
+
+    usize i = 0;
+    usize j = len - 1;
+    while (i < j) {
         char temp = str[i];
         str[i] = str[j];
         str[j] = temp;
+        i++;
+        j--;
     }
 }
 
-
-// 去除字符串首尾的空格、\t、\n等空白字符
-char* str_trim(char* str)
+bool str_starts_with(const char* str, const char* prefix)
 {
-    if (str == NULL)
-        return NULL;
-    // 找到字符串开始的非空白字符
-    char* p_start = str;
-    while (*p_start != '\0' && (*p_start == ' ' || *p_start == '\t' || *p_start == '\n')) {
-        p_start++;
+    if (!str || !prefix) {
+        return false;
     }
 
-    // 找到字符串末尾的非空白字符
-    char* p_end = str + strlen(str) - 1;
-    while (p_end > p_start && (*p_end == ' ' || *p_end == '\t' || *p_end == '\n')) {
-        p_end--;
-    }
-
-    // 将非空白字符移动到字符串的开始，并在末尾添加字符串终止符
-    memmove(str, p_start, p_end - p_start + 1);
-    str[p_end - p_start + 1] = '\0';
-
-    return str;
-}
-
-// 字符串分割（类似 strtok）
-char* str_tok(char* str, const char* delim)
-{
-    // 如果str不为NULL，则开始新的字符串解析
-    if (str) {
-        strtok_pos = str;
-    } 
-    // 如果str为NULL且strtok_pos为NULL，则返回NULL
-    else if (!strtok_pos) {
-        return NULL;
-    }
-    
-    // 跳过起始的分隔符
-    while (*strtok_pos) {
-        if (!str_chr(delim, *strtok_pos)) {
-            break;
+    while (*prefix) {
+        if (*str != *prefix) {
+            return false;
         }
-        strtok_pos++;
+        str++;
+        prefix++;
     }
-    
-    // 如果到达字符串末尾，返回NULL
-    if (*strtok_pos == '\0') {
-        strtok_pos = NULL;
-        return NULL;
-    }
-    
-    // 找到token的起始位置
-    char* token_start = strtok_pos;
-    
-    // 查找下一个分隔符
-    while (*strtok_pos) {
-        if (str_chr(delim, *strtok_pos)) {
-            *strtok_pos = '\0';  // 将分隔符替换为字符串终止符
-            strtok_pos++;        // 移动到下一个字符
-            return token_start;
-        }
-        strtok_pos++;
-    }
-    
-    // 到达字符串末尾，没有更多分隔符
-    strtok_pos = NULL;
-    return token_start;
+
+    return true;
 }
 
-///////////////////////////////////////////////////////////////////////////////
-
-// 字符串拷贝
-char* str_copy(char* dest, const char* src)
+bool str_starts_with_i(const char* str, const char* prefix)
 {
-    while (*src != '\0') {
-        *dest = *src;
-        dest++;
-        src++;
+    if (!str || !prefix) {
+        return false;
     }
-    // 补一个\0
-    *dest = '\0';
-    return dest;
+
+    while (*prefix) {
+        if (char_to_lower(*str) != char_to_lower(*prefix)) {
+            return false;
+        }
+        str++;
+        prefix++;
+    }
+
+    return true;
 }
 
+bool str_ends_with(const char* str, const char* suffix)
+{
+    if (!str || !suffix) {
+        return false;
+    }
 
+    usize str_len_val = str_len(str);
+    usize suffix_len = str_len(suffix);
 
+    if (suffix_len > str_len_val) {
+        return false;
+    }
 
+    const char* str_end = str + str_len_val - suffix_len;
+    while (*suffix) {
+        if (*str_end != *suffix) {
+            return false;
+        }
+        str_end++;
+        suffix++;
+    }
+
+    return true;
+}
+
+bool str_ends_with_i(const char* str, const char* suffix)
+{
+    if (!str || !suffix) {
+        return false;
+    }
+
+    usize str_len_val = str_len(str);
+    usize suffix_len = str_len(suffix);
+
+    if (suffix_len > str_len_val) {
+        return false;
+    }
+
+    const char* str_end = str + str_len_val - suffix_len;
+    while (*suffix) {
+        if (char_to_lower(*str_end) != char_to_lower(*suffix)) {
+            return false;
+        }
+        str_end++;
+        suffix++;
+    }
+
+    return true;
+}
 
 #if TEST_ENABLE
-
 #include "../em_test/test.h"
 
-TEST_CASE(test_str_len_case)
+TEST_CASE(test_char_to_lower)
 {
-    TEST_ASSERT_EQUAL_INT(str_len("abc"), 3);
-    TEST_ASSERT_EQUAL_INT(str_len(""), 0);
+    TEST_ASSERT_EQUAL_INT('a', char_to_lower('A'));
+    TEST_ASSERT_EQUAL_INT('a', char_to_lower('a'));
+    TEST_ASSERT_EQUAL_INT('1', char_to_lower('1'));
 }
 
-TEST_CASE(test_str_cpy_case)
+TEST_CASE(test_char_to_upper)
+{
+    TEST_ASSERT_EQUAL_INT('A', char_to_upper('a'));
+    TEST_ASSERT_EQUAL_INT('A', char_to_upper('A'));
+    TEST_ASSERT_EQUAL_INT('1', char_to_upper('1'));
+}
+
+TEST_CASE(test_str_len)
+{
+    TEST_ASSERT_EQUAL_UINT(0, str_len(NULL));
+    TEST_ASSERT_EQUAL_UINT(0, str_len(""));
+    TEST_ASSERT_EQUAL_UINT(5, str_len("hello"));
+}
+
+TEST_CASE(test_str_nlen)
+{
+    TEST_ASSERT_EQUAL_UINT(0, str_nlen(NULL, 10));
+    TEST_ASSERT_EQUAL_UINT(5, str_nlen("hello", 10));
+    TEST_ASSERT_EQUAL_UINT(3, str_nlen("hello", 3));
+    TEST_ASSERT_EQUAL_UINT(0, str_nlen("hello", 0));
+}
+
+TEST_CASE(test_str_cpy)
 {
     char buf[10];
-    str_cpy(buf, "hello", sizeof(buf));
-    TEST_ASSERT_EQUAL_STRING(buf, "hello");
+    // 正常拷贝
+    TEST_ASSERT(str_cpy(buf, "hello", 10) == 5);
+    TEST_ASSERT_EQUAL_STRING("hello", buf);
+    
+    // 溢出检查
+    TEST_ASSERT(str_cpy(buf, "hello world", 5) == -1);
+    
+    // 边界：刚好装满 (4字符 + \0)
+    TEST_ASSERT(str_cpy(buf, "1234", 5) == 4);
+    
+    // 异常输入
+    TEST_ASSERT(str_cpy(NULL, "test", 10) == -1);
+    TEST_ASSERT(str_cpy(buf, NULL, 10) == -1);
+    TEST_ASSERT(str_cpy(buf, "test", 0) == -1);
 }
 
-TEST_CASE(test_str_cmp_case)
+TEST_CASE(test_str_cat)
 {
-    TEST_ASSERT_EQUAL_INT(str_cmp("abc", "abc", 3), 0);
+    char buf[20] = "hello";
+    // 正常拼接
+    TEST_ASSERT(str_cat(buf, " world", 20) == 11);
+    TEST_ASSERT_EQUAL_STRING("hello world", buf);
+    
+    // 溢出检查
+    TEST_ASSERT(str_cat(buf, " again", 12) == -1);
+    
+    // 异常输入
+    TEST_ASSERT(str_cat(NULL, "test", 10) == -1);
+    TEST_ASSERT(str_cat(buf, NULL, 10) == -1);
+    TEST_ASSERT(str_cat(buf, "test", 0) == -1);
+    
+    // 目标缓冲区无空间或无终止符
+    char full_buf[5] = "12345"; // 实际上已经越界了，模拟非法状态
+    TEST_ASSERT(str_cat(full_buf, "test", 5) == -1);
+}
+
+TEST_CASE(test_str_cmp)
+{
+    TEST_ASSERT_EQUAL_INT(0, str_cmp("abc", "abc", 3));
     TEST_ASSERT(str_cmp("abc", "abd", 3) < 0);
+    TEST_ASSERT(str_cmp("abd", "abc", 3) > 0);
+    TEST_ASSERT_EQUAL_INT(0, str_cmp("abc", "abcd", 3));
+    
+    // 边缘情况
+    TEST_ASSERT_EQUAL_INT(0, str_cmp(NULL, "abc", 3));
+    TEST_ASSERT_EQUAL_INT(0, str_cmp("abc", NULL, 3));
+    TEST_ASSERT_EQUAL_INT(0, str_cmp("abc", "abc", 0));
 }
 
-TEST_CASE(test_str_chr_case)
+TEST_CASE(test_str_is_equal)
 {
-    TEST_ASSERT(str_chr("hello", 'e') != NULL);
-    TEST_ASSERT(str_chr("hello", 'z') == NULL);
+    TEST_ASSERT_TRUE(str_is_equal("abc", "abc"));
+    TEST_ASSERT_FALSE(str_is_equal("abc", "abd"));
+    TEST_ASSERT_FALSE(str_is_equal("abc", "abcd"));
+    
+    // 指针相等
+    const char* s = "ptr";
+    TEST_ASSERT_TRUE(str_is_equal(s, s));
+    
+    // NULL 检查
+    TEST_ASSERT_FALSE(str_is_equal(NULL, "abc"));
+    TEST_ASSERT_FALSE(str_is_equal("abc", NULL));
 }
 
-TEST_CASE(test_str_str_case)
+TEST_CASE(test_str_find_ch)
 {
-    TEST_ASSERT(str_str("abcdef", "cd") != NULL);
-    TEST_ASSERT(str_str("abcdef", "gh") == NULL);
+    const char* s = "hello";
+    TEST_ASSERT(str_find_ch(s, 'e') == s + 1);
+    TEST_ASSERT(str_find_ch(s, 'z') == NULL);
+    TEST_ASSERT(str_find_ch(s, '\0') == s + 5);
+    TEST_ASSERT(str_find_ch(NULL, 'a') == NULL);
 }
 
-TEST_CASE(test_str_tok_case)
+TEST_CASE(test_str_find_str)
 {
-    char  buf2[32] = "a,b,c";
-    char* token    = str_tok(buf2, ",");
-    TEST_ASSERT_EQUAL_STRING(token, "a");
-    token = str_tok(NULL, ",");
-    TEST_ASSERT_EQUAL_STRING(token, "b");
-    token = str_tok(NULL, ",");
-    TEST_ASSERT_EQUAL_STRING(token, "c");
-    token = str_tok(NULL, ",");
-    TEST_ASSERT(token == NULL);
+    const char* s = "hello world";
+    TEST_ASSERT(str_find_str(s, "world") == s + 6);
+    TEST_ASSERT(str_find_str(s, "hello") == s);
+    TEST_ASSERT(str_find_str(s, "earth") == NULL);
+    
+    // 边缘情况
+    TEST_ASSERT(str_find_str(s, "") == s);
+    TEST_ASSERT(str_find_str(NULL, "test") == NULL);
+    TEST_ASSERT(str_find_str("test", NULL) == NULL);
 }
 
-TEST_CASE(test_str_to_upper_case)
+TEST_CASE(test_str_trim)
 {
-    char buf3[16] = "abcDEF";
-    str_to_upper(buf3);
-    TEST_ASSERT_EQUAL_STRING(buf3, "ABCDEF");
+    char s1[] = "  hello  ";
+    TEST_ASSERT(str_trim(s1) == 5);
+    TEST_ASSERT_EQUAL_STRING("hello", s1);
+
+    char s2[] = "\t\n world \r";
+    TEST_ASSERT(str_trim(s2) == 5);
+    TEST_ASSERT_EQUAL_STRING("world", s2);
+    
+    // 全空格/空字符串
+    char s3[] = "   ";
+    TEST_ASSERT(str_trim(s3) == 0);
+    TEST_ASSERT_EQUAL_STRING("", s3);
+    
+    char s4[] = "";
+    TEST_ASSERT(str_trim(s4) == 0);
+    
+    TEST_ASSERT(str_trim(NULL) == 0);
 }
 
-TEST_CASE(test_str_to_lower_case)
+TEST_CASE(test_str_to_upper)
 {
-    char buf4[16] = "ABCdef";
-    str_to_lower(buf4);
-    TEST_ASSERT_EQUAL_STRING(buf4, "abcdef");
+    char s[] = "hello123";
+    str_to_upper(s);
+    TEST_ASSERT_EQUAL_STRING("HELLO123", s);
+    str_to_upper(NULL); // 异常检查
 }
 
-TEST_CASE(test_str_reverse_case)
+TEST_CASE(test_str_to_lower)
 {
-    char buf5[16] = "abcdef";
-    str_reverse(buf5);
-    TEST_ASSERT_EQUAL_STRING(buf5, "fedcba");
+    char s[] = "HELLO123";
+    str_to_lower(s);
+    TEST_ASSERT_EQUAL_STRING("hello123", s);
+    str_to_lower(NULL); // 异常检查
 }
 
-TEST_CASE(test_hex_str_to_uint_case)
+TEST_CASE(test_str_reverse)
 {
-    u32 val = 0;
-    TEST_ASSERT(hex_str_to_uint("1A3F", &val));
-    TEST_ASSERT_EQUAL_INT(val, 0x1A3F);
-    TEST_ASSERT(!hex_str_to_uint("xyz", &val));
+    char s1[] = "hello";
+    str_reverse(s1);
+    TEST_ASSERT_EQUAL_STRING("olleh", s1);
+    
+    char s2[] = "a";
+    str_reverse(s2);
+    TEST_ASSERT_EQUAL_STRING("a", s2);
+    
+    str_reverse(NULL);
 }
 
-TEST_CASE(test_uint_to_hex_str_case)
+TEST_CASE(test_str_starts_with)
 {
-    char buf6[9];
-    uint_to_hex_str(0x1A3F, buf6, sizeof(buf6));
-    TEST_ASSERT_EQUAL_STRING(buf6, "1A3F");
+    TEST_ASSERT_TRUE(str_starts_with("hello", "he"));
+    TEST_ASSERT_FALSE(str_starts_with("hello", "ha"));
+    TEST_ASSERT_FALSE(str_starts_with(NULL, "he"));
 }
 
+TEST_CASE(test_str_starts_with_i)
+{
+    TEST_ASSERT_TRUE(str_starts_with_i("hello", "HE"));
+    TEST_ASSERT_FALSE(str_starts_with_i("hello", "HA"));
+    TEST_ASSERT_FALSE(str_starts_with_i("hello", NULL));
+}
+
+TEST_CASE(test_str_ends_with)
+{
+    TEST_ASSERT_TRUE(str_ends_with("hello", "lo"));
+    TEST_ASSERT_FALSE(str_ends_with("hello", "la"));
+    TEST_ASSERT_FALSE(str_ends_with("he", "hello")); // 长度不足
+    TEST_ASSERT_FALSE(str_ends_with("hello", NULL));
+}
+
+TEST_CASE(test_str_ends_with_i)
+{
+    TEST_ASSERT_TRUE(str_ends_with_i("hello", "LO"));
+    TEST_ASSERT_FALSE(str_ends_with_i("hello", "LA"));
+    TEST_ASSERT_FALSE(str_ends_with_i("he", "hello"));
+}
 #endif
-
-
-// #include <libca/core/string.h>
-// #include <stdio.h>
-// #include <libca/core/test.h>
-
-// TEST_CASE(str_trim)
-// {
-//     char buf[512] = "  \n \t 21345\n\t ";
-//     printf("%s1", str_trim(buf));
-// }
-
-// // int main(int argc, char* argv[])
-// // {
-// //     test1();
-// //     return 0;
-// // }
--- src/em_base/string_util.h
+++ src/em_base/string_util.h
@@ -12,73 +12,201 @@
 #define LIBCA_EM_BASE_STRING_UTIL_H
 
 #include "datatype.h"
-#include <stdbool.h>
 
-// 字符串长度（类似 strlen，但更安全）
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief 字符转小写
+ * 
+ * @param c 字符
+ * @return char 转小写的字符
+ */
+char char_to_lower(char c);
+
+/**
+ * @brief 字符转大写
+ * 
+ * @param c 字符
+ * @return char 转大写的字符
+ */
+char char_to_upper(char c);
+
+/**
+ * @brief 获取字符串长度
+ * 
+ * @param str 字符串指针
+ * @return usize 字符串长度
+ */
 usize str_len(const char* str);
 
+/**
+ * @brief 获取字符串长度（带最大长度限制，更安全）
+ * 
+ * @param str 字符串指针
+ * @param max_len 最大检查长度
+ * @return usize 字符串长度
+ */
 usize str_nlen(const char* str, usize max_len);
 
-// 字符串复制（类似 strncpy，但确保终止符），大于0时，返回实际复制的字符串长度，小于0时，返回的是错误码
+/**
+ * @brief 字符串复制（确保以 \0 结尾）
+ * 
+ * @param dest 目标缓冲区
+ * @param src 源字符串
+ * @param size 目标缓冲区总大小
+ * @return i32 成功返回实际复制的长度，失败返回错误码
+ */
 i32 str_cpy(char* dest, const char* src, usize size);
 
-// 字符串拼接，大于0时，返回实际复制的字符串长度，小于0时，返回的是错误码
+/**
+ * @brief 字符串拼接（确保以 \0 结尾）
+ * 
+ * @param dest 目标缓冲区
+ * @param src 源字符串
+ * @param dest_max_size 目标缓冲区总大小
+ * @return i32 成功返回实际拼接后的长度，失败返回错误码
+ */
 i32 str_cat(char* dest, const char* src, usize dest_max_size);
 
-// 字符串比较（类似 strncmp）
+/**
+ * @brief 比较两个字符串
+ * 
+ * @param s1 字符串1
+ * @param s2 字符串2
+ * @param size 最大比较长度
+ * @return i32 相等返回 0，不等返回差值
+ */
 i32 str_cmp(const char* s1, const char* s2, usize size);
 
-// 对str_cmp的一个封装，方便if (str_is_equal(s1, "START")) 这种判断
+/**
+ * @brief 判断两个字符串是否完全相等
+ * 
+ * @param s1 字符串1
+ * @param s2 字符串2
+ * @return bool 相等返回 true，否则返回 false
+ */
 bool str_is_equal(const char* s1, const char* s2);
 
-// 查找字符（类似 strchr）
+/**
+ * @brief 在字符串中查找字符
+ * 
+ * @param str 字符串
+ * @param c 要查找的字符
+ * @return char* 指向查找到的字符的指针，未找到返回 NULL
+ */
 char* str_find_ch(const char* str, char c);
 
-// 查找子串（类似 strstr）
+/**
+ * @brief 在字符串中查找子串
+ * 
+ * @param haystack 主字符串
+ * @param needle 要查找的子串
+ * @return char* 指向查找到的子串的指针，未找到返回 NULL
+ */
 char* str_find_str(const char* haystack, const char* needle);
 
-// 去除字符串首尾的空格、\t、\n等空白字符，原地实现，返回去除以后字符串的长度
+/**
+ * @brief 去除字符串首尾的空白字符（原地实现）
+ * 
+ * @param str 字符串
+ * @return i32 去除后的字符串长度
+ */
 i32 str_trim(char* str);
 
-// 字符串转大写
+/**
+ * @brief 字符串转大写
+ * 
+ * @param str 字符串
+ */
 void str_to_upper(char* str);
 
-// 字符串转小写
+/**
+ * @brief 字符串转小写
+ * 
+ * @param str 字符串
+ */
 void str_to_lower(char* str);
 
-// 反转字符串
+/**
+ * @brief 翻转字符串
+ * 
+ * @param str 字符串
+ */
 void str_reverse(char* str);
 
-// 判断是否以某个字符串开始
+/**
+ * @brief 判断字符串是否以特定前缀开始
+ * 
+ * @param str 字符串
+ * @param prefix 前缀
+ * @return bool 是返回 true
+ */
 bool str_starts_with(const char* str, const char* prefix);
 
-// 判断是否以某个字符串开始（忽略大小写）
+/**
+ * @brief 判断字符串是否以特定前缀开始（忽略大小写）
+ * 
+ * @param str 字符串
+ * @param prefix 前缀
+ * @return bool 是返回 true
+ */
 bool str_starts_with_i(const char* str, const char* prefix);
 
-// 判断是否以某个字符串结尾
+/**
+ * @brief 判断字符串是否以特定后缀结尾
+ * 
+ * @param str 字符串
+ * @param suffix 后缀
+ * @return bool 是返回 true
+ */
 bool str_ends_with(const char* str, const char* suffix);
 
-// 判断是否以某个字符串结尾（忽略大小写）
-bool str_end_with_i(const char* str, const char* suffix);
+/**
+ * @brief 判断字符串是否以特定后缀结尾（忽略大小写）
+ * 
+ * @param str 字符串
+ * @param suffix 后缀
+ * @return bool 是返回 true
+ */
+bool str_ends_with_i(const char* str, const char* suffix);
 
+
+#if 0
 ///////////////////////////////////////////////////////////////////////////////
-// 内存设置
-void* mem_set(void* ptr, uint8_t value, uint32_t num);
-// 内存复制
-void* mem_copy(void* dest, const void* src, uint32_t num);
-// 内存移动
-void* mem_move(void* dest, const void* src, uint32_t num);
-// 内存比较
-int32_t mem_compare(const void* ptr1, const void* ptr2, uint32_t num);
+// 内存操作
+void* mem_set(void* ptr, u8 value, u32 num);
+void* mem_copy(void* dest, const void* src, u32 num);
+void* mem_move(void* dest, const void* src, u32 num);
+i32 mem_compare(const void* ptr1, const void* ptr2, u32 num);
 
 ///////////////////////////////////////////////////////////////////////////////
 // 十六进制转换
-char* to_hex(const void* data, uint32_t data_len, char* buf, uint32_t buf_len);
+char* to_hex(const void* data, u32 data_len, char* buf, u32 buf_len);
 
-// 十六进制字符串转整数
+/**
+ * @brief 十六进制字符串转整数
+ * 
+ * @param str 字符串
+ * @param out_value 输出值
+ * @return bool 成功返回 true
+ */
 bool hex_str_to_uint(const char* str, u32* out_value);
 
-// 整数转十六进制字符串
+/**
+ * @brief 整数转十六进制字符串
+ * 
+ * @param value 整数
+ * @param out_str 输出缓冲区
+ * @param out_size 缓冲区大小
+ */
 void uint_to_hex_str(u32 value, char* out_str, usize out_size);
+
+#endif
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif   // !LIBCA_EM_BASE_STRING_UTIL_H
