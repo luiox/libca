3f0f110c50c131d69848b83d411f68f561a374fd
Canrad <1517807724@qq.com>
2026-01-23 20:32:17 +0800

    修复缺失的文件头说明

--- src/em_driver/ir_track.h
+++ src/em_driver/ir_track.h
@@ -1,3 +1,13 @@
+/**
+ * @file ir_track.h
+ * @author canrad (1517807724@qq.com)
+ * @brief 反射式寻迹的驱动
+ * @version 0.1
+ * @date 2026-01-23
+ * 
+ * @copyright Copyright (c) 2026
+ * 
+ */
 #ifndef LIBCA_EM_DRIVER_IR_TRACK_H
 #define LIBCA_EM_DRIVER_IR_TRACK_H
 
--- src/em_driver/simple_digital_sensor_template.h
+++ src/em_driver/simple_digital_sensor_template.h
@@ -1,3 +1,13 @@
+/**
+ * @file simple_digital_sensor_template.h
+ * @author canrad (1517807724@qq.com)
+ * @brief 简单读取io的驱动模板生成宏工具
+ * @version 0.1
+ * @date 2026-01-23
+ * 
+ * @copyright Copyright (c) 2026
+ * 
+ */
 #ifndef LIBCA_EM_DRIVER_SIMPLE_DIGITAL_SENSOR_TEMPLATE_H
 #define LIBCA_EM_DRIVER_SIMPLE_DIGITAL_SENSOR_TEMPLATE_H
 
@@ -7,43 +17,36 @@
 #define SIMPLE_DIGITAL_SENSOR_TEMPLATE_DEFINE_HEADER(sensor_name, get_value_func_name)        \
     typedef struct CONNECT2(sensor_name, _port)                                               \
     {                                                                                         \
-        u8   (*read_pin)(void* gpio, u16 pin);                                   \
+        u8 (*read_pin)(void* gpio, u16 pin);                                                  \
     } CONNECT2(sensor_name, _port_t);                                                         \
     void CONNECT2(sensor_name, _bind_port)(const CONNECT2(sensor_name, _port_t) * port);      \
     bool CONNECT2(sensor_name, _port_is_registered)(void);                                    \
-                                                                                              \
     typedef struct sensor_name                                                                \
     {                                                                                         \
         void* gpio;                                                                           \
         u16   pin;                                                                            \
     } CONNECT2(sensor_name, _t);                                                              \
-                                                                                              \
     void CONNECT2(sensor_name, _init)(CONNECT2(sensor_name, _t) * self, void* gpio, u16 pin); \
-    u8 CONNECT3(sensor_name, _, get_value_func_name)(CONNECT2(sensor_name, _t) * self);
+    u8   CONNECT3(sensor_name, _, get_value_func_name)(CONNECT2(sensor_name, _t) * self);
 
-#define SIMPLE_DIGITAL_SENSOR_TEMPLATE_DEFINE_SOURCE(sensor_name, get_value_func_name) \
-\
-static const CONNECT2(sensor_name, _port_t) * CONNECT3(g_, sensor_name, _port) = NULL;\
-\
-void CONNECT2(sensor_name, _bind_port)(const CONNECT2(sensor_name, _port_t) * port)\
-{\
-    CONNECT3(g_, sensor_name, _port) = port;\
-}\
-\
-bool CONNECT2(sensor_name, _port_is_registered)(void)\
-{\
-    return CONNECT3(g_, sensor_name, _port) != NULL;\
-}\
-\
-void CONNECT2(sensor_name, _init)(CONNECT2(sensor_name, _t) * self, void* gpio, u16 pin)\
-{\
-    self->gpio = gpio;\
-    self->pin  = pin;\
-}\
-\
-u8 CONNECT3(sensor_name, _, get_value_func_name)(CONNECT2(sensor_name, _t) * self)\
-{\
-    return CONNECT3(g_, sensor_name, _port)->read_pin(self->gpio, self->pin);\
-}\
+#define SIMPLE_DIGITAL_SENSOR_TEMPLATE_DEFINE_SOURCE(sensor_name, get_value_func_name)       \
+    static const CONNECT2(sensor_name, _port_t) * CONNECT3(g_, sensor_name, _port) = NULL;   \
+    void CONNECT2(sensor_name, _bind_port)(const CONNECT2(sensor_name, _port_t) * port)      \
+    {                                                                                        \
+        CONNECT3(g_, sensor_name, _port) = port;                                             \
+    }                                                                                        \
+    bool CONNECT2(sensor_name, _port_is_registered)(void)                                    \
+    {                                                                                        \
+        return CONNECT3(g_, sensor_name, _port) != NULL;                                     \
+    }                                                                                        \
+    void CONNECT2(sensor_name, _init)(CONNECT2(sensor_name, _t) * self, void* gpio, u16 pin) \
+    {                                                                                        \
+        self->gpio = gpio;                                                                   \
+        self->pin  = pin;                                                                    \
+    }                                                                                        \
+    u8 CONNECT3(sensor_name, _, get_value_func_name)(CONNECT2(sensor_name, _t) * self)       \
+    {                                                                                        \
+        return CONNECT3(g_, sensor_name, _port)->read_pin(self->gpio, self->pin);            \
+    }
 
 #endif   // !LIBCA_EM_DRIVER_SIMPLE_DIGITAL_SENSOR_TEMPLATE_H
