ed1b93350fca70599e24c116d5c75757fc4b02c8
Canrad <1517807724@qq.com>
2026-01-31 15:56:39 +0800

    把还不能明确的先放在源文件使用#if 0禁用

--- src/em_base/macro_util.h
+++ src/em_base/macro_util.h
@@ -1,8 +1,23 @@
+/**
+ * @file macro_util.h
+ * @author canrad (1517807724@qq.com)
+ * @brief 宏工具的封装
+ * @version 0.2
+ * @date 2026-01-18
+ * @update 2026-01-31 明确已有的宏
+ *
+ * @copyright Copyright (c) 2026
+ * 
+ */
 #ifndef LIBCA_EM_BASE_MACRO_UTIL_H
 #define LIBCA_EM_BASE_MACRO_UTIL_H 
 
 #define MAKE_STRING(a) #a
+#ifdef __COUNTER__
+#define UNIQUE_ID __COUNTER__
+#else
 #define UNIQUE_ID __LINE__
+#endif
 
 // 各种数量的连接宏
 #define __CONNECT2(__A, __B) __A##__B
@@ -26,7 +41,8 @@
   __CONNECT7(__A, __B, __C, __D, __E, __F, __G)
 #define CONNECT8(__A, __B, __C, __D, __E, __F, __G, __H) \
   __CONNECT8(__A, __B, __C, __D, __E, __F, __G, __H)
-#define CONNECT9(__A, __B, __C, __D, __E, __F, __G, __H, __I)
+#define CONNECT9(__A, __B, __C, __D, __E, __F, __G, __H, __I) \
+  __CONNECT9(__A, __B, __C, __D, __E, __F, __G, __H, __I)
 
 
 #define __PLOOC_VA_NUM_ARGS_IMPL(_0, _1, _2, _3, _4, _5, _6, _7, _8, _9, _10, \
@@ -57,44 +73,6 @@
  */
 #define EVAL(__FUNC, ...) CONNECT2(__FUNC, VA_NUM_ARGS(__VA_ARGS__))
 
-#define __USING_1(__declare) \
-  for (__declare, *SAFE_NAME(using_ptr) = NULL; SAFE_NAME(using_ptr)++ == NULL;)
 
-#define __USING_2(__declare, __on_leave_expr)   \
-  for (__declare, *SAFE_NAME(using_ptr) = NULL; \
-       SAFE_NAME(using_ptr)++ == NULL; __on_leave_expr)
-
-#define __USING_3(__declare, __on_enter_expr, __on_leave_expr)      \
-  for (__declare, *SAFE_NAME(using_ptr) = NULL;                     \
-       SAFE_NAME(using_ptr)++ == NULL ? ((__on_enter_expr), 1) : 0; \
-       __on_leave_expr)
-
-#define __USING_4(__dcl1, __dcl2, __on_enter_expr, __on_leave_expr) \
-  for (__dcl1, __dcl2, *SAFE_NAME(using_ptr) = NULL;                \
-       SAFE_NAME(using_ptr)++ == NULL ? ((__on_enter_expr), 1) : 0; \
-       (__on_leave_expr))
-
-/**
- * @brief 局部变量
- * @param __declare 局部变量
- * @param __on_enter_expr 进入操作 [可选]
- * @param __on_leave_expr 离开操作 [可选]
- */
-#define USING(...) EVAL(__USING_, __VA_ARGS__)(__VA_ARGS__)
-
-// 编译时断言验证特定表达式。
-// 如果表达式求值为零，编译将失败
-// 只能用于整数类型
-#define COMPILE_TIME_ASSERT_IMPL(exp, random_variable_name)                                                          \
-    typedef char random_variable_name[!(exp) ? -1 : 1];
-
-#define COMPILE_TIME_ASSERT(exp)                                                          \
-    COMPILE_TIME_ASSERT_IMPL(exp, SAFE_NAME(assert_var))
-// demo
-// void f()
-// {
-//   COMPILE_TIME_ASSERT(0==0);
-
-// }
 
 #endif // !LIBCA_EM_BASE_MACRO_UTIL_H
