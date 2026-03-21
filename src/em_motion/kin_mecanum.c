6357efebfbe5ff128d2780b90ddb5b04630b217f
Canrad <1517807724@qq.com>
2026-03-21 22:30:05 +0800

    feat(em_motion): 为 kin_mecanum 添加正运动学和里程计
    - 添加 mecanum_odometry_t 结构体
    - 实现 mecanum_fk 正运动学（从轮速推算底盘速度）
    - 实现 mecanum_odometry_init 和 mecanum_odometry_update
    - 更新头文件注释说明轮子布局
    
    Fixes #92 P2-mid (mecanum部分)

--- src/em_motion/kin_mecanum.c
+++ src/em_motion/kin_mecanum.c
@@ -1,9 +1,9 @@
 /**
  * @file kin_mecanum.c
  * @author canrad (1517807724@qq.com)
- * @brief 麦克纳姆底盘运动学基础实现
- * @version 0.1
- * @date 2026-03-16
+ * @brief 麦克纳姆底盘运动学实现
+ * @version 0.2
+ * @date 2026-03-21
  *
  * @copyright Copyright (c) 2026
  *
@@ -11,18 +11,88 @@
 
 #include "kin_mecanum.h"
 
+#include <math.h>
+
 void mecanum_ik(f32 vx, f32 vy, f32 wz, const mecanum_param_t* p, f32 wheel_speed[4])
 {
-    f32 k;
-
     if (!p || !wheel_speed || p->wheel_radius <= 0.0f) {
         return;
     }
 
-    k = p->half_length + p->half_width;
+    f32 r = p->wheel_radius;
+    f32 k = p->half_length + p->half_width;
 
-    wheel_speed[0] = (vx - vy - k * wz) / p->wheel_radius;
-    wheel_speed[1] = (vx + vy + k * wz) / p->wheel_radius;
-    wheel_speed[2] = (vx + vy - k * wz) / p->wheel_radius;
-    wheel_speed[3] = (vx - vy + k * wz) / p->wheel_radius;
+    wheel_speed[0] = (vx - vy - k * wz) / r;
+    wheel_speed[1] = (vx + vy + k * wz) / r;
+    wheel_speed[2] = (vx + vy - k * wz) / r;
+    wheel_speed[3] = (vx - vy + k * wz) / r;
+}
+
+void mecanum_fk(const f32 wheel_speed[4], const mecanum_param_t* p, f32* vx, f32* vy, f32* wz)
+{
+    if (!wheel_speed || !p || p->wheel_radius <= 0.0f) {
+        if (vx)
+            *vx = 0.0f;
+        if (vy)
+            *vy = 0.0f;
+        if (wz)
+            *wz = 0.0f;
+        return;
+    }
+
+    f32 r   = p->wheel_radius;
+    f32 k   = p->half_length + p->half_width;
+    f32 wFL = wheel_speed[0];
+    f32 wFR = wheel_speed[1];
+    f32 wRL = wheel_speed[2];
+    f32 wRR = wheel_speed[3];
+
+    if (vx) {
+        *vx = r * (wFL + wFR + wRL + wRR) / 4.0f;
+    }
+    if (vy) {
+        *vy = r * (-wFL + wFR + wRL - wRR) / 4.0f;
+    }
+    if (wz) {
+        if (k > 0.0f) {
+            *wz = r * (-wFL + wFR - wRL + wRR) / (4.0f * k);
+        }
+        else {
+            *wz = 0.0f;
+        }
+    }
+}
+
+void mecanum_odometry_init(mecanum_odometry_t* odo)
+{
+    if (!odo) {
+        return;
+    }
+    odo->x     = 0.0f;
+    odo->y     = 0.0f;
+    odo->theta = 0.0f;
+}
+
+void mecanum_odometry_update(mecanum_odometry_t* odo, f32 vx, f32 vy, f32 wz, f32 dt)
+{
+    if (!odo || dt <= 0.0f) {
+        return;
+    }
+
+    f32 cos_theta = cosf(odo->theta);
+    f32 sin_theta = sinf(odo->theta);
+
+    f32 vx_global = vx * cos_theta - vy * sin_theta;
+    f32 vy_global = vx * sin_theta + vy * cos_theta;
+
+    odo->x += vx_global * dt;
+    odo->y += vy_global * dt;
+    odo->theta += wz * dt;
+
+    while (odo->theta > 3.14159265f) {
+        odo->theta -= 2.0f * 3.14159265f;
+    }
+    while (odo->theta < -3.14159265f) {
+        odo->theta += 2.0f * 3.14159265f;
+    }
 }
--- src/em_motion/kin_mecanum.h
+++ src/em_motion/kin_mecanum.h
@@ -2,12 +2,17 @@
  * @file kin_mecanum.h
  * @author canrad (1517807724@qq.com)
  * @brief 麦克纳姆底盘运动学接口
- * @version 0.1
- * @date 2026-03-16
+ * @version 0.2
+ * @date 2026-03-21
  *
  * @copyright Copyright (c) 2026
  *
+ * 四轮麦克纳姆布局（俯视）：
+ *   FL  FR  (前)
+ *   RL  RR  (后)
+ * 轮子辊子方向：FL/RR 指向右后，FR/RL 指向左后
  */
+
 #ifndef LIBCA_EM_MOTION_KIN_MECANUM_H
 #define LIBCA_EM_MOTION_KIN_MECANUM_H
 
@@ -17,28 +22,30 @@
 extern "C" {
 #endif
 
-/**
- * @brief 麦克纳姆底盘几何参数
- */
-typedef struct mecanum_param_s {
+typedef struct mecanum_param_s
+{
     f32 wheel_radius;
     f32 half_length;
     f32 half_width;
 } mecanum_param_t;
 
-/**
- * @brief 麦克纳姆逆运动学
- *
- * @param vx x 方向线速度，单位 m/s
- * @param vy y 方向线速度，单位 m/s
- * @param wz 角速度，单位 rad/s
- * @param p 运动学参数
- * @param wheel_speed 输出轮速数组，顺序 FL/FR/RL/RR，单位 rad/s
- */
+typedef struct mecanum_odometry_s
+{
+    f32 x;
+    f32 y;
+    f32 theta;
+} mecanum_odometry_t;
+
 void mecanum_ik(f32 vx, f32 vy, f32 wz, const mecanum_param_t* p, f32 wheel_speed[4]);
+
+void mecanum_fk(const f32 wheel_speed[4], const mecanum_param_t* p, f32* vx, f32* vy, f32* wz);
+
+void mecanum_odometry_init(mecanum_odometry_t* odo);
+
+void mecanum_odometry_update(mecanum_odometry_t* odo, f32 vx, f32 vy, f32 wz, f32 dt);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif // !LIBCA_EM_MOTION_KIN_MECANUM_H
+#endif
