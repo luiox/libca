42a780085405a75d8bd93d1d75b0e25c60bc29ea
Canrad <1517807724@qq.com>
2026-03-21 22:29:07 +0800

    feat(em_motion): 为 kin_omni 添加正运动学和里程计
    - 添加 omni3_odometry_t 结构体
    - 实现 omni3_fk 正运动学（从轮速推算底盘速度）
    - 实现 omni3_odometry_init 和 omni3_odometry_update
    - 更新头文件注释说明轮子布局
    
    Fixes #92 P2-mid (omni部分)

--- src/em_motion/kin_omni.h
+++ src/em_motion/kin_omni.h
@@ -1,13 +1,21 @@
 /**
  * @file kin_omni.h
  * @author canrad (1517807724@qq.com)
- * @brief 全向轮（3/4轮）运动学接口
- * @version 0.1
- * @date 2026-03-16
+ * @brief 全向轮（3轮）运动学接口
+ * @version 0.2
+ * @date 2026-03-21
  *
  * @copyright Copyright (c) 2026
  *
+ * 三轮全向轮布局（俯视）：
+ *       W2 (后方)
+ *        |
+ *   W0 /   \ W1
+ *     /_____\
+ *     前方
+ * 轮子方向：垂直于到圆心的连线，正方向为逆时针切向
  */
+
 #ifndef LIBCA_EM_MOTION_KIN_OMNI_H
 #define LIBCA_EM_MOTION_KIN_OMNI_H
 
@@ -17,27 +25,29 @@
 extern "C" {
 #endif
 
-/**
- * @brief 三轮全向底盘参数
- */
-typedef struct omni3_param_s {
+typedef struct omni3_param_s
+{
     f32 wheel_radius;
     f32 chassis_radius;
 } omni3_param_t;
 
-/**
- * @brief 三轮全向逆运动学
- *
- * @param vx x 方向线速度，单位 m/s
- * @param vy y 方向线速度，单位 m/s
- * @param wz 角速度，单位 rad/s
- * @param p 运动学参数
- * @param wheel_speed 输出轮速数组（长度 3），单位 rad/s
- */
+typedef struct omni3_odometry_s
+{
+    f32 x;
+    f32 y;
+    f32 theta;
+} omni3_odometry_t;
+
 void omni3_ik(f32 vx, f32 vy, f32 wz, const omni3_param_t* p, f32 wheel_speed[3]);
+
+void omni3_fk(const f32 wheel_speed[3], const omni3_param_t* p, f32* vx, f32* vy, f32* wz);
+
+void omni3_odometry_init(omni3_odometry_t* odo);
+
+void omni3_odometry_update(omni3_odometry_t* odo, f32 vx, f32 vy, f32 wz, f32 dt);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif // !LIBCA_EM_MOTION_KIN_OMNI_H
+#endif
