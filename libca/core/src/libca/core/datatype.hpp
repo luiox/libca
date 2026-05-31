fee546e800981da0874909543f18cb6f74f72ff0
Canrad <1517807724@qq.com>
2026-05-31 12:10:56 +0800

    明确类型语义

--- libca/core/src/libca/core/datatype.hpp
+++ libca/core/src/libca/core/datatype.hpp
@@ -2,17 +2,22 @@
 #include <cstdint>
 
 namespace ca {
-    using i8  = std::int8_t;
-    using i16 = std::int16_t;
-    using i32 = std::int32_t;
-    using i64 = std::int64_t;
-    using u8  = std::uint8_t;
-    using u16 = std::uint16_t;
-    using u32 = std::uint32_t;
-    using u64 = std::uint64_t;
+    // 整数语义类型
+    using i8  = int8_t;
+    using i16 = int16_t;
+    using i32 = int32_t;
+    using i64 = int64_t;
+    using u8  = uint8_t;
+    using u16 = uint16_t;
+    using u32 = uint32_t;
+    using u64 = uint64_t;
+    // 浮点数语义类型
     using f32 = float;
     using f64 = double;
+    // 明确的大小语义类型
     using usize = size_t;
+    // 明确的字节语义，避免与std::byte那种不方便的类型混淆，也避免跟u8这种8位无符号整数混淆语义
+    using byte = u8;
 
     inline namespace literals {
         constexpr i32 operator""_i32(unsigned long long v) noexcept { return static_cast<i32>(v); }
